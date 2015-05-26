b440330a8b74ee75dd13c6761b0776e27aad1fa8
jbkr <baker.james.jb@gmail.com>
2015-05-26 15:15:26 +0100

    Added script that allows easy cross over to the group bin for generic scripts

--- pdb_to_multifasta.rb
+++ pdb_to_multifasta.rb
@@ -1,17 +1,17 @@
-#Converts each pdb entry in a directory into a multi FASTA format where each domain is a new FASTA entry.
+#Converts each pdb entry in a directory into a multi FASTA format where each domain is a new FASTA entry. Good for alignments etc.
+
+#A problem with the bioruby pdb sequence extractor was that it required an argument for which domain to convert to fasta. Here the conversion uses bioruby and then further parses to produce a FASTA with domains having their own FASTA entry without needing an argument.
 
 #Gem for bioruby.
 require 'bio'
-
 time = Time.new
+
+#This line sets the current directory as the one to convert pdbs.
 path = Dir.pwd
-
-#Change this to the folder where your PDBs are being kept.
 Dir.glob("#{path}/*.pdb") do |pdb_file| #opens each pdb file in the folder and performs the below function on it.
-
-  filename = pdb_file
-  string = IO.read(filename)
+  string = IO.read(pdb_file)
   structure = Bio::PDB.new(string)
+  #This line is the title of the output. Usually it'll come out as "output_2015-05-25 02:18:37 +0100.fasta" or similar.
   output_file = "#{path}/output_#{time}.fasta"
 
   for domain in structure.seqres #The seqres currently holds several IDs and they don't properly print.
@@ -22,6 +22,7 @@ Dir.glob("#{path}/*.pdb") do |pdb_file| #opens each pdb file in the folder and p
     puts domain[1] #Holds the actual sequence for the domain.
     puts
 
+    #This is the actual content that is added to the file.
     content = "#{titleline} #{domain[0]}\n #{domain[1]}\n\n "
     File.open(output_file, 'a') { |file| file.write(content) }
 
