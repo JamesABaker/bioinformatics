4da30018ce9c0ca02bde3b774c1d43e53ac565f1
jbkr <baker.james.jb@gmail.com>
2015-05-26 16:57:32 +0100

    Random DNA maker in python

---
+++ random_DNA.py
@@ -0,0 +1,28 @@
+import socket
+import random
+HOST, PORT = '', 8888
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print 'Serving HTTP on port %s ...' % PORT
+while True:
+    client_connection, client_address = listen_socket.accept()
+    request = client_connection.recv(1024)
+    print request
+
+    #Script goes here.
+    dna_list = ["A","T","C","G"]
+    organism_dna_list = []
+
+    mut = random.choice(dna_list)
+    length = len(mut)
+    while length < 200:
+        mut = mut + random.choice(dna_list)
+        length = len(mut)
+    
+    
+    http_response = ">random_fasta\n" + mut #This holds the output.
+    client_connection.sendall(http_response)
+    client_connection.close()
