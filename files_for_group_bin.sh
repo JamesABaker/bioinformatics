83d5ec2125e40f55ff85d40a0eef59a46ec32567
jbkr <baker.james.jb@gmail.com>
2015-06-25 17:38:11 +0100

    removed the group uploader file. Depreciated.

--- files_for_group_bin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Below add the directories for transferring to the group git from my personal pool.
-
-cp Users/James/Documents/My\ Scripts/work/TM_bioinformatics/pdb_to_multifasta.rb Users/James/Documents/My\ Scripts/work/group_bin/pdb_to_multifasta.rb
