2bf2b8bd963479429f0a257e9703505566ff36af
jbkr <baker.james.jb@gmail.com>
2015-10-29 14:12:31 +0800

    Updated to support multi file output

--- dat_to_fasta.py
+++ dat_to_fasta.py
@@ -1,23 +1,42 @@
-#This script aims at analysing the relationship between the hydrophobicity of the flanking regions and the hydropbobicity of the TMH.
+# This script aims at analysing the relationship between the
+# hydrophobicity of the flanking regions and the hydropbobicity of the
+# TMH.
 
-#PYTHON3
+# PYTHON3
 
-#This requires a working version of Biopython.
+# This requires a working version of Biopython.
 from Bio import SeqIO
 import numpy
 
 ### BIOPYTHON STANDARD VARIABLES FOR SEQ-IO ###
 
 
-#These are the variables that are repeatedly used throughout the script. The only one that changes is the output_filename that is changed as the separate fasta sequences are generated.
-filenames = [".txt"]
-input_format = "swiss" #This SHOULD work with uniprot filetype
-feature_type = "TRANSMEM" #For future modification, this can be used to look for any annotation in the .dat file.
-output_filename = "TMH.fasta" #Simply the output name, can be anything as it is written in binary (not file-type specific language).
+# These are the variables that are repeatedly used throughout the script.
+# The only one that changes is the output_filename that is changed as the
+# separate fasta sequences are generated.
+filenames = ["helixnottransmembraneandhuman.txt"]
+input_format = "swiss"  # This SHOULD work with uniprot filetype
+# For future modification, this can be used to look for any annotation in
+# the .dat file.
+feature_type = "HELIX"
+# Simply the output name, can be anything as it is written in binary (not
+# file-type specific language).
+output_filename = "Helix.fasta"
 
 
+with open("output/flank1.fasta", "wb") as myfile:
+    myfile.write("")
 
-#This list will hold the IDs and the average KD
+
+with open("output/TMD.fasta", "wb") as myfile:
+    myfile.write("")
+
+
+with open("output/flank2.fasta", "ab") as myfile:
+    myfile.write("")
+
+
+# This list will hold the IDs and the average KD
 
 
 total_average_KD = []
@@ -28,17 +47,42 @@ total_average_KD = []
 output = open(output_filename, "w")
 for filename in filenames:
     # Using SeqIO.parse will cope with multi-record files
-    for record in SeqIO.parse(filename, input_format): #A biopython module that can automatically parse uniprot .txt files
+    # A biopython module that can automatically parse uniprot .txt files
+    for record in SeqIO.parse(filename, input_format):
 
         total_average_KD = []
-        TMD_counter=0
+        TMD_counter = 0
         for x, f in enumerate(record.features):
 
-            #feature type refers to TRANSMEM
+            # feature type refers to TRANSMEM
             if f.type == feature_type:
-                TMD_counter = TMD_counter+1
-
+                TMD_counter = TMD_counter + 1
 
                 TMD = f.extract(record.seq)
-                print(">", record.id, record.description, record.name)
-                print(TMD)
+                flank1 = record.seq[(f.location.start - 5):(f.location.start)]
+                flank2 = record.seq[(f.location.end):(f.location.end + 5)]
+
+                header = str(">" + record.id +
+                             record.description + record.name)
+                print header
+                print flank1, TMD, flank2
+                flank1 = str(flank1)
+                flank2 = str(flank2)
+                TMD = str(TMD)
+                with open("output/flank1.fasta", "ab") as myfile:
+                    myfile.write(header)
+                    myfile.write("\n")
+                    myfile.write(flank1)
+                    myfile.write("\n")
+
+                with open("output/TMD.fasta", "ab") as myfile:
+                    myfile.write(header)
+                    myfile.write("\n")
+                    myfile.write(TMD)
+                    myfile.write("\n")
+
+                with open("output/flank2.fasta", "ab") as myfile:
+                    myfile.write(header)
+                    myfile.write("\n")
+                    myfile.write(flank2)
+                    myfile.write("\n")
